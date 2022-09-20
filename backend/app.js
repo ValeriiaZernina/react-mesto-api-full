6e76cb8748fe1568cbebf76429fb1bd66efcf718
Valeria Zernina <darovanie@mail.ru>
2022-09-21 00:06:48 +0500

    backend

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
-const { celebrate, Joi, errors } = require('celebrate');
-const { StatusNotFound } = require('./utils/errors/StatusNotFound');
-const { handleError } = require('./utils/handleError');
-const { createUser, login } = require('./controllers/users');
-const auth = require('./middleware/auth');
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const { celebrate, Joi, errors } = require("celebrate");
+const { StatusNotFound } = require("./utils/errors/StatusNotFound");
+const { handleError } = require("./utils/handleError");
+const { createUser, login } = require("./controllers/users");
+const auth = require("./middleware/auth");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 // запуск на 3000 порту
 const { PORT = 3000 } = process.env;
@@ -13,47 +14,61 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 // подключаемся к серверу mongo
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect("mongodb://localhost:27017/mestodb", {
   useNewUrlParser: true,
 });
+
+// подключаем логгер запросов
+app.use(requestLogger);
+
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Сервер сейчас упадёт");
+  }, 0);
+});
+
 // получение куки
 app.use(cookieParser());
 // обновление, вместо bodyParser
 app.use(express.json());
 // роуты, не требующие авторизации, регистрация и логин
 app.post(
-  '/signin',
+  "/signin",
   celebrate({
     body: Joi.object().keys({
       email: Joi.string().email().required(),
       password: Joi.string().required(),
     }),
   }),
-  login,
+  login
 );
 app.post(
-  '/signup',
+  "/signup",
   celebrate({
     body: Joi.object().keys({
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
       avatar: Joi.string().pattern(
-        /https?:\/\/(www\.)?([-a-zA-Z0-9()@:%_+.~#?&=]*)\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/,
+        /https?:\/\/(www\.)?([-a-zA-Z0-9()@:%_+.~#?&=]*)\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/
       ),
       password: Joi.string().required(),
       email: Joi.string().email().required(),
     }),
   }),
-  createUser,
+  createUser
 );
 
-app.use('/users', auth, require('./routes/users'));
-app.use('/cards', auth, require('./routes/cards'));
+app.use("/users", auth, require("./routes/users"));
+app.use("/cards", auth, require("./routes/cards"));
 
-app.all('*', auth, (req, res, next) => {
-  next(new StatusNotFound('Не существующий маршрут'));
+app.all("*", auth, (req, res, next) => {
+  next(new StatusNotFound("Не существующий маршрут"));
 });
 
+// подключаем логгер ошибок
+app.use(errorLogger);
+
+// обработчик ошибок celebrate
 app.use(errors());
 app.use(handleError);
 
